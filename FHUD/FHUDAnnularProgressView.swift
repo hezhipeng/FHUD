4edd4d89287ece4412c293ad520fd4e7450ab073
贺智鹏 <ECEJ@hezhipengdeMacBook-Pro.local>
2018-05-15 11:22:59 +0800

    添加GradientRotation

---
+++ FHUD/FHUDAnnularProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  FHUDAnnularProgressView.swift
+//  FHUD
+//
+//  Created by Frank.he on 2018/5/14.
+//  Copyright © 2018年 一城一家网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class FHUDAnnularProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FHUD/FHUDGradientRotationView.swift
@@ -0,0 +1,125 @@
+//
+//  FHUDSemicircleRotationView.swift
+//  FHUD
+//
+//  Created by Frank.he on 2018/5/14.
+//  Copyright © 2018年 一城一家网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class FHUDSemicircleRotationView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.addSublayer(topGradientLayer)
+        self.layer.addSublayer(bottomGradientLayer)
+
+        self.layer.mask = maskLayer
+        DispatchQueue.main.async {
+            self.rotationAnimation()
+        }
+    }
+    
+    deinit {
+        print(#file+" "+#function)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        topGradientLayer.frame = CGRect(x: 0, y: 0, width: self.bounds.maxX, height: self.bounds.midY)
+        bottomGradientLayer.frame = CGRect(x: 0, y: self.bounds.midY, width: self.bounds.maxX, height: self.bounds.midY)
+
+        maskLayer.path = path().cgPath
+    }
+    
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+        self.layer.removeAllAnimations()
+    }
+    
+    override var tintColor: UIColor? {
+        didSet {
+            if let tintColor = tintColor {
+//                let colorTop = tintColor.cgColor
+//                let colorBottom = tintColor.withAlphaComponent(0.1).cgColor
+//                gradientLayer.colors = [colorTop, colorBottom]
+//                gradientLayer.locations = [0.0, 1.0]
+//                gradientLayer.frame = self.view.bounds
+//
+//
+//                self.semicircleGradientLayer.strokeColor = tintColor.cgColor
+            }
+        }
+    }
+    
+    private func path() -> UIBezierPath {
+        
+        let center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        let radius = self.bounds.maxY / 2.0 - 3
+        let start = -1/4*CGFloat(2.0 * Double.pi)
+        let end = CGFloat(2.0 * Double.pi)
+        
+        return UIBezierPath.init(arcCenter: center,
+                                 radius: radius,
+                                 startAngle: start,
+                                 endAngle: end,
+                                 clockwise: true)
+    }
+    
+    func rotationAnimation() {
+        
+        let rotationAnimation = CABasicAnimation.init(keyPath: "transform.rotation.z")
+        rotationAnimation.fromValue = 0
+        rotationAnimation.toValue = 2.0 * Double.pi
+        rotationAnimation.duration = 1
+        rotationAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        rotationAnimation.repeatCount = HUGE
+        self.layer.add(rotationAnimation, forKey: "rotationAnimation")
+    }
+    
+    lazy var topGradientLayer: CAGradientLayer = {
+        
+        //颜色渐变
+        let colors = [UIColor.white.cgColor, UIColor.white.withAlphaComponent(0.5).cgColor]
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.shadowPath = path().cgPath
+        gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.colors = colors
+        return gradientLayer
+    }()
+    
+    lazy var bottomGradientLayer: CAGradientLayer = {
+        
+        //颜色渐变
+        let colors = [UIColor.white.withAlphaComponent(0.5).cgColor, UIColor.white.withAlphaComponent(0.0).cgColor]
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.shadowPath = path().cgPath
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.colors = colors
+        return gradientLayer
+    }()
+    
+    
+    lazy var maskLayer: CAShapeLayer = {
+        
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = UIColor.white.cgColor
+        layer.lineJoin = kCALineCapRound
+        layer.lineWidth = 3;
+        layer.strokeStart = 0;
+        layer.strokeEnd = 1;
+        layer.lineCap = kCALineCapRound;
+        layer.lineDashPhase = 0.8;
+        
+        return layer
+    }()
+    
+}
