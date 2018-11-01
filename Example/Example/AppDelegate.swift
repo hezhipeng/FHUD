27d14d7f0062c10ab43996f70529d305c3b12323
贺智鹏 <ECEJ@hezhipengdeMacBook-Pro.local>
2018-11-01 17:08:20 +0800

    swift4.2

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        FHUD.appearance().titleColor = .red
 //        FHUD.appearance().titleFont = UIFont.systemFont(ofSize: 15)
 //        FHUD.appearance().contextViewBackgroundColor = .yellow   
